923b20b690179f8150c3b557f9341cd5942d0ba4
Pea <pea@misfist.com>
2024-04-24 18:07:19 -0400

    Config

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'b68051825308492749097c5db23e95ca10a910fc',
         'name' => 'misfist/site-functionality',
         'dev' => false,
     ),
     'versions' => array(
         'misfist/site-functionality' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'b68051825308492749097c5db23e95ca10a910fc',
             'dev_requirement' => false,
         ),
     ),
